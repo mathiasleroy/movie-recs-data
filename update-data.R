080b989ab9cc67cfb776020d2a3cb004dbfc6d13
Mathias Leroy <mathiasleroy@users.noreply.github.com>
2026-01-05 19:56:01 +0100

    comment tests

--- update-data.R
+++ update-data.R
@@ -17,7 +17,7 @@ source("R/get_tmdb.R")
 
 ### CREATE QS FILES -----
 
-message("## 1. data/df_imdb_sm_{date}.qs ---")
+message("## 1. data/df_imdb_sm.qs ---")
 load_imdb(what = "all", force_update = TRUE)
 
 message("## 2. data/df_movielens.qs ---")
@@ -28,15 +28,20 @@ do_use <- "imdb"
 do_reverse <- 0
 maxtofetch <- 200
 if (do_use == "recs" & file.exists("data/df_recs.qs")) {
-    message("- use recs")
-    df_tofetch <- qs::qread("data/df_recs.qs")
-    if (do_reverse) df_tofetch <- df_tofetch %>% arrange(rating)
+  message("- use recs")
+  df_tofetch <- qs::qread("data/df_recs.qs")
+  if (do_reverse) df_tofetch <- df_tofetch %>% arrange(rating)
 } else {
-    message("- use imdb")
-    if (!exists("df_imdb_sm")) df_imdb_sm <- qs::qread("data/df_imdb_sm.qs")
-    df_tofetch <- df_imdb_sm %>%
-        arrange(numVotes |> desc()) %>%
-        select(tconst, numVotes)
-    if (do_reverse) df_tofetch <- df_tofetch %>% arrange(numVotes)
+  message("- use imdb")
+  if (!exists("df_imdb_sm")) df_imdb_sm <- qs::qread("data/df_imdb_sm.qs") ## contains series too
+  df_tofetch <- df_imdb_sm %>%
+    arrange(numVotes |> desc()) %>%
+    select(tconst, numVotes)
+  if (do_reverse) df_tofetch <- df_tofetch %>% arrange(numVotes)
 }
 batch_scrape_tmdb_api(df_tofetch, limit_to_fetch = maxtofetch)
+
+
+## check
+# df_tmdb <- qs::qread("data/df_tmdb.qs")
+# df_tmdb %>% count(type, sort = T)
